f8ece0e48620430f0eeaa4e3adc4f9dc2f02bd74
saiyashwanth <saiyashwanth1991@gmail.com>
2017-09-20 13:35:41 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 public class Main {
 
 	public static void main(String[] args) {
@@ -5,6 +6,8 @@ public class Main {
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
+		Date d=new Date(11,8,1996);
+		Student s=new Student(558,"yashwanth",d,50);
 	}
 
 }
